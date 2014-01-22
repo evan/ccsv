8dc70d630844d708971bb807c9027e86c32b58f1
Sergey Zhumatiy <serg@parallel.ru>
2014-01-22 09:15:26 +0400

    - version 1.0.3 (rspec-style benchmarks added)

--- Gemfile
+++ Gemfile
@@ -4,4 +4,5 @@
 
 group :development do
 	gem 'echoe'
+	gem 'minitest'
 end
---
+++ spec/bench_spec.rb
@@ -0,0 +1,43 @@
+require 'bundler'
+Bundler.require(:default, :test)
+
+require 'minitest/autorun'
+require 'minitest/benchmark'
+require 'ccsv'
+require 'csv'
+
+TEST_CSV_FILES={
+	100=>"/tmp/test1.csv",
+	10000=>"/tmp/test2.csv",
+	100000=>"/tmp/test3.csv"
+}
+
+def create_csvf(name,limit)
+	open(name,"w") do |f|
+        	1.upto(limit) do |n|
+			f.puts [n,2*n,3+n].join(',')
+		end
+	end
+end
+
+describe "Ccsv Benchmark" do
+  bench_range { [100,10_000,100_000] } 
+  before do
+	  [100,10_000,100_000].each do |n|
+		  create_csvf(TEST_CSV_FILES[n],n)
+	  end
+  end
+
+  after do
+	  [100,10_000,100_000].each do |n|
+		  FileUtils.rm TEST_CSV_FILES[n]
+	  end
+  end
+
+  bench_performance_linear "Ccsv", 0.8 do |n|
+	Ccsv.foreach(TEST_CSV_FILES[n]) do |x| end
+  end
+  bench_performance_linear "CSV", 0.8 do |n|
+	CSV.foreach(TEST_CSV_FILES[n]) do |x| end
+  end
+end
